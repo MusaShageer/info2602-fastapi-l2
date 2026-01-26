66bd2b1e77428a713309be5c4076940696f6e936
MusaShageer <musashageer018@gmail.com>
2026-01-26 17:48:54 -0400

    new update
    new update

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .database import create_db_and_tables
+from app.database import create_db_and_tables
 app = FastAPI()
 
 
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlmodel import Field, SQLModel
 from typing import Optional
 from pwdlib import PasswordHash
+from pydantic import field_validator
 
 password_hash = PasswordHash.recommended()
 
@@ -9,3 +10,20 @@ class User(SQLModel, table=True):
     username:str = Field(index=True, unique=True)
     email:str = Field(index=True, unique=True)
     password:str
+
+    # def __init__(self, username, email, password):
+    #     self.username = username
+    #     self.email = email
+    #     self.set_password(password)
+    
+    # def set_password(self, password):
+    @field_validator('password', mode='before')
+    @classmethod
+    def hash_password(cls, v):
+        if isinstance(v, str):
+            return password_hash.hash(v)
+        return v
+        self.password = password_hash.hash(password)
+
+    def __str__(self) -> str:
+        return f"(User id={self.id}, username={self.username} ,email={self.email})"
